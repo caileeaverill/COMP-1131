fc3095e98977fe93ecf4cd509cd13b9a5dcfc466
Cailee Averill <caileeaverill@gmail.com>
2023-11-26 14:32:06 -0500

    latest files

--- Assignments/Assignment_3_Averill_Cailee/numberGuessingGame.java
+++ Assignments/Assignment_3_Averill_Cailee/numberGuessingGame.java
@@ -8,10 +8,10 @@ public class numberGuessingGame {
     String answer = "";
 
     do {
-      askForGuess();
+      runGame();
       System.out.print("Do you want to play again?(y/n)");
       answer = yesOrNo.nextLine();
-      System.out.println(answer);
+      // System.out.println(answer);
     } while (answer.equals("y"));
 
     if (answer.equals("n")) {
@@ -19,56 +19,55 @@ public class numberGuessingGame {
     }
   }
 
-  public static void askForGuess() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a character: ");
-    String guess = scanner.nextLine();
-
-    Random r = new Random();
-    char randomLetter = (char) (r.nextInt(26) + 'a');
-
-    System.out.println("Random letter: " + randomLetter);
-
-    runGame(guess, randomLetter);
-  }
-
   public static void Congratulations() {
     System.out.println("Congratulations! You guessed the letter!");
-  }
-
-  public static void wrongGuess(char randomLetter) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a character: ");
-    String guess = scanner.nextLine();
-    runGame(guess, randomLetter);
   }
 
   public static void Loser(char randomLetter) {
     System.out.println("You lose! The letter was " + randomLetter);
   }
 
-  public static void runGame(String guess, char randomLetter) {
+  public static void runGame() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a character: ");
+    String guess = scanner.nextLine();
+    char guessChar = guess.charAt(0);
+
     if (guess.matches("[a-zA-Z]+\\.?")) {
-      Character guessChar = guess.charAt(0);
       int numOfGuesses = 25;
 
-      System.out.println("Guess character: " + guessChar);
+      Random r = new Random();
+      char randomLetter = (char) (r.nextInt(26) + 'a');
+
       System.out.println("Random letter: " + randomLetter);
+
+      int tries = 0;
 
       for (int i = 0; i < numOfGuesses; numOfGuesses--) {
         if (guessChar == randomLetter) {
           Congratulations();
           break;
-        } else if (guessChar.equals(randomLetter)) {
-          wrongGuess(randomLetter);
-          System.out.println("running!");
+        } else if (guessChar != randomLetter) {
+
+          do {
+            System.out.println("Wrong! Please try again!");
+            System.out.print("Enter a character: ");
+            guess = scanner.nextLine();
+            guessChar = guess.charAt(0);
+            tries++;
+            System.out.println(tries);
+          } while (tries < 3);
+
+          if (tries > 3) {
+            System.out.println("Keep playing?");
+          }
         } else if (guessChar != randomLetter || numOfGuesses >= 0) {
           Loser(randomLetter);
         }
       }
     } else {
       System.out.println("Error: invalid character");
-      askForGuess();
+      runGame();
     }
   }
 }
